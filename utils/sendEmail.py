9a78a0c09349b80d4e9150703cf7ada10cfcd88f
monicaranganadane24-coder <monicaranganadane24@gmail.com>
2026-06-21 13:14:44 +0200

    update backend URL

--- utils/sendEmail.py
+++ utils/sendEmail.py
@@ -1,25 +1,23 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 async def send_confirmation_email(to_email: str, subject: str, content: str):
     try:
-        smtp_user     = os.getenv("GMAIL_USER")     # monicaranganadane24@gmail.com
-        smtp_password = os.getenv("GMAIL_PASSWORD") # mot de passe d'application Google
+        api_key     = os.getenv("SENDGRID_API_KEY")
+        sender_email = os.getenv("SENDER_EMAIL")
 
-        msg = MIMEMultipart("alternative")
-        msg["Subject"] = subject
-        msg["From"]    = smtp_user
-        msg["To"]      = to_email
+        message = Mail(
+            from_email=sender_email,
+            to_emails=to_email,
+            subject=subject,
+            html_content=content
+        )
 
-        msg.attach(MIMEText(content, "html"))
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(smtp_user, smtp_password)
-            server.sendmail(smtp_user, to_email, msg.as_string())
-
-        print(f"✅ Mail envoyé à {to_email}")
+        print(f"✅ Mail envoyé à {to_email} (status {response.status_code})")
         return True
 
     except Exception as e:
